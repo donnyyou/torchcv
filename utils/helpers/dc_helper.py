32933b7b9e8a651b698760bce3e7b571daed1b01
Donny You <youansheng@pku.edu.cn>
2019-03-12 23:38:02 +0800

    fix dchelper.

--- utils/helpers/dc_helper.py
+++ utils/helpers/dc_helper.py
@@ -3,6 +3,7 @@
 # Author: Donny You(youansheng@gmail.com)
 
 
+import torch
 import itertools
 
 from extensions.tools.parallel import DataContainer
@@ -12,12 +13,18 @@ class DCHelper(object):
 
     @staticmethod
     def tolist(dc):
-        return list(itertools.chain(*dc.data))
+        if dc.samples_per_gpu and not dc.stack:
+            return list(itertools.chain(*dc.data))
+        else:
+            return dc.data
 
     @staticmethod
-    def todc(data_list, samples_per_gpu, cpu_only=False):
+    def todc(data_list, samples_per_gpu, stack=False, cpu_only=False):
         stacked = []
         for i in range(0, len(data_list), samples_per_gpu):
-            stacked.append(data_list[i:i + samples_per_gpu])
+            if not stack:
+                stacked.append(data_list[i:i + samples_per_gpu])
+            else:
+                stacked.append(torch.cat(data_list[i:i + samples_per_gpu], 0))
 
-        return DataContainer(stacked, cpu_only=cpu_only)
+        return DataContainer(stacked, stack=stack, cpu_only=cpu_only)
