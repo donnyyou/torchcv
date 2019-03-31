629873bf47f75cd410993928139a8bec09e9b5d8
Donny You <youansheng@pku.edu.cn>
2019-03-31 21:38:54 +0800

    fix bug.

--- datasets/gan/preprocess/face/casia_nir2vis.py
+++ datasets/gan/preprocess/face/casia_nir2vis.py
@@ -7,8 +7,8 @@ def trans_gt(protocols_dir, new_protocols_dir):
     for tag in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'dev']:
         train_vis_file = 'vis_train_{}.txt'.format(tag)
         train_nir_file = 'nir_train_{}.txt'.format(tag)
-        val_vis_file = 'vis_gallery_{}'.format(tag)
-        val_nir_file = 'nir_probe_{}'.format(tag)
+        val_vis_file = 'vis_gallery_{}.txt'.format(tag)
+        val_nir_file = 'nir_probe_{}.txt'.format(tag)
 
         label_id = 0
         train_label_dict = dict()
@@ -40,12 +40,12 @@ def trans_gt(protocols_dir, new_protocols_dir):
         with open(os.path.join(protocols_dir, val_nir_file), 'r') as fr:
             for line in fr.readlines():
                 name = line.strip().split('\\')[-2]
-                if name not in train_label_dict:
+                if name not in val_label_dict:
                     val_label_dict[name] = label_id
                     label_id += 1
 
                 valA_json_list.append(
-                    dict(image_path=line.strip().replace('\\', '/'), label=val_label_dict['name'], name=name)
+                    dict(image_path=line.strip().replace('\\', '/'), label=val_label_dict[name], name=name)
                 )
 
         with open(os.path.join(protocols_dir, val_vis_file), 'r') as fr:
@@ -53,7 +53,7 @@ def trans_gt(protocols_dir, new_protocols_dir):
                 name = line.strip().split('\\')[-2]
                 assert name in val_label_dict
                 valB_json_list.append(
-                    dict(image_path=line.strip().replace('\\', '/'), label=val_label_dict['name'], name=name)
+                    dict(image_path=line.strip().replace('\\', '/'), label=val_label_dict[name], name=name)
                 )
 
         fw = open(os.path.join(new_protocols_dir, 'train_label{}A.json'.format(tag)), 'w')
@@ -78,4 +78,7 @@ if __name__ == "__main__":
     parser.add_argument('--new_protocols_dir', default=None, type=str,
                         dest='new_protocols_dir', help='The directory of protocols.')
     args = parser.parse_args()
+    if not os.path.exists(args.new_protocols_dir):
+        os.makedirs(args.new_protocols_dir)
+
     trans_gt(args.protocols_dir, args.new_protocols_dir)
