f67c38a79d3a5d85dc8b341cbfb2c2c869e55122
Donny You <youansheng@pku.edu.cn>
2019-05-27 00:36:02 +0800

    clean scripts.

--- scripts/pose/coco/run_openpose_vgg19_coco_pose.sh
+++ scripts/pose/coco/run_openpose_vgg19_coco_pose.sh
@@ -34,9 +34,6 @@ elif [[ "$1"x == "resume"x ]]; then
                        --resume_continue y --resume ./checkpoints/det/coco/${CHECKPOINTS_NAME}_latest.pth \
                        --checkpoints_name ${CHECKPOINTS_NAME} --pretrained ${PRETRAINED_MODEL}  2>&1 | tee -a ${LOG_FILE}
 
-elif [[ "$1"x == "debug"x ]]; then
-  ${PYTHON} -u main.py --hypes ${HYPES_FILE} --phase debug --gpu 0 --log_to_file n  2>&1 | tee ${LOG_FILE}
-
 elif [[ "$1"x == "val"x ]]; then
   ${PYTHON} -u main.py --hypes ${HYPES_FILE} --phase test --log_to_file n --model_name ${MODEL_NAME} \
                        --phase test --gpu 0 --resume ./checkpoints/cityscapes/${CHECKPOINTS_NAME}_latest.pth \
