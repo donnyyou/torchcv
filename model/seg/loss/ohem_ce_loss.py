31844b1acb8d7d9203fd9d9d9e78c34e7b4565aa
Donny You <youansheng@pku.edu.cn>
2019-08-03 14:49:52 +0800

    fix ohem ce.

--- model/seg/loss/ohem_ce_loss.py
+++ model/seg/loss/ohem_ce_loss.py
@@ -6,6 +6,7 @@
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class OhemCELoss(nn.Module):
@@ -13,12 +14,11 @@ class OhemCELoss(nn.Module):
         super(OhemCELoss, self).__init__()
         self.configer = configer
         weight = self.configer.get('loss.params.ohem_ce_loss.weight', default=None)
-        weight = torch.FloatTensor(weight) if weight is not None else weight
-        reduction = self.configer.get('loss.params.ohem_ce_loss.reduction', default='mean')
-        ignore_index = self.configer.get('loss.params.ohem_ce_loss.ignore_index', default=-100)
+        self.weight = torch.FloatTensor(weight) if weight is not None else weight
+        self.reduction = self.configer.get('loss.params.ohem_ce_loss.reduction', default='mean')
+        self.ignore_index = self.configer.get('loss.params.ohem_ce_loss.ignore_index', default=-100)
         self.thresh = self.configer.get('loss.params.ohem_ce_loss.thresh', default=0.7)
         self.min_kept = max(1, self.configer.get('loss.params.ohem_ce_loss.minkeep', default=1))
-        self.ignore_label = ignore_index
 
     def forward(self, predict, target):
         """
@@ -28,16 +28,17 @@ class OhemCELoss(nn.Module):
                 weight (Tensor, optional): a manual rescaling weight given to each class.
                                            If given, has to be a Tensor of size "nclasses"
         """
+        batch_kept = self.min_kept * target.size(0)
         target = self._scale_target(target, (predict.size(2), predict.size(3)))
         prob_out = F.softmax(predict, dim=1)
         tmp_target = target.clone()
-        tmp_target[tmp_target == self.ignore_label] = 0
+        tmp_target[tmp_target == self.ignore_index] = 0
         prob = prob_out.gather(1, tmp_target.unsqueeze(1))
-        mask = target.contiguous().view(-1, ) != self.ignore_label
+        mask = target.contiguous().view(-1, ) != self.ignore_index
         sort_prob, sort_indices = prob.contiguous().view(-1, )[mask].contiguous().sort()
-        min_threshold = sort_prob[min(self.min_kept, sort_prob.numel() - 1)] if sort_prob.numel() > 0 else 0.0
+        min_threshold = sort_prob[min(batch_kept, sort_prob.numel() - 1)] if sort_prob.numel() > 0 else 0.0
         threshold = max(min_threshold, self.thresh)
-        loss_matrix = F.cross_entropy(predict, target, weight=self.weight.to(input.device),
+        loss_matrix = F.cross_entropy(predict, target, weight=self.weight.to(predict.device),
                                       ignore_index=self.ignore_index, reduction='none')
         loss_matirx = loss_matrix.contiguous().view(-1, )
         sort_loss_matirx = loss_matirx[mask][sort_indices]
