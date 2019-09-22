16280fae454b0412a3b78735d1170b13ddd365bd
Donny You <youansheng@pku.edu.cn>
2019-09-22 14:45:55 +0800

    fix model.

--- model/cls/nets/cls_model.py
+++ model/cls/nets/cls_model.py
@@ -50,16 +50,18 @@ class ClsModel(nn.Module):
             self.linear_list.append(LINEAR_DICT[linear_type](fc_dim, num_classes))
 
         self.embed = None
-        if configer.get('network.{}embed'.format(self.flag), default=True):
+        if configer.get('network.{}embed'.format(self.flag), default=False):
             feat_dim = configer.get('network', '{}feat_dim'.format(self.flag))
             self.embed = nn.Sequential(
                 nn.Linear(fc_dim, feat_dim),
                 nn.BatchNorm1d(feat_dim)
             )
 
-        self.bn = nn.BatchNorm1d(fc_dim)
-        nn.init.zeros_(self.bn.bias)
-        self.bn.bias.requires_grad = False
+        self.bn = None
+        if configer.get('network.{}bn'.format(self.flag), default=False):
+            self.bn = nn.BatchNorm1d(fc_dim)
+            nn.init.zeros_(self.bn.bias)
+            self.bn.bias.requires_grad = False
 
         self.valid_loss_dict = LOSS_TYPE[configer.get('loss', 'loss_type')] if loss_dict is None else loss_dict
 
@@ -67,37 +69,35 @@ class ClsModel(nn.Module):
         out_dict = dict()
         label_dict = dict()
         loss_dict = dict()
-        x_ = self.backbone(data_dict['img'])
-        if 'img' in data_dict:
-            x = x_[:data_dict['img'].size(0)]
-            x = F.adaptive_avg_pool2d(x, 1)
-            x = self.reduction(x) if self.reduction else x
-            x = x.view(x.size(0), -1)
-            fc = self.bn(x)
-            for i in range(len(self.linear_list)):
-                sub_out = self.linear_list[i](fc, data_dict['label'][:, i])
-                out_dict['{}out{}'.format(self.flag, i)] = sub_out
-                label_dict['{}out{}'.format(self.flag, i)] = data_dict['label'][:, i]
-                if 'ce_loss{}'.format(i) in self.valid_loss_dict:
-                    loss_dict['{}ce_loss{}'.format(self.flag, i)] = dict(
-                        params=[sub_out, data_dict['label'][:, i]],
-                        type=torch.cuda.LongTensor([BASE_LOSS_DICT['ce_loss']]),
-                        weight=torch.cuda.FloatTensor([self.valid_loss_dict['ce_loss{}'.format(i)]])
-                    )
+        x = self.backbone(data_dict['img'])
+        x = F.adaptive_avg_pool2d(x, 1)
+        x = self.reduction(x) if self.reduction else x
+        x = x.view(x.size(0), -1)
+        fc = self.bn(x) if self.bn else x
+        for i in range(len(self.linear_list)):
+            sub_out = self.linear_list[i](fc, data_dict['label'][:, i])
+            out_dict['{}out{}'.format(self.flag, i)] = sub_out
+            label_dict['{}out{}'.format(self.flag, i)] = data_dict['label'][:, i]
+            if 'ce_loss{}'.format(i) in self.valid_loss_dict:
+                loss_dict['{}ce_loss{}'.format(self.flag, i)] = dict(
+                    params=[sub_out, data_dict['label'][:, i]],
+                    type=torch.cuda.LongTensor([BASE_LOSS_DICT['ce_loss']]),
+                    weight=torch.cuda.FloatTensor([self.valid_loss_dict['ce_loss{}'.format(i)]])
+                )
 
-            feat = self.embed(x) if self.embed else x
-            for i in range(len(self.linear_list)):
-                if 'tri_loss{}'.format(i) in self.valid_loss_dict:
-                    loss_dict['{}tri_loss{}'.format(self.flag, i)] = dict(
-                        params=[feat, data_dict['label'][:, i]],
-                        type=torch.cuda.LongTensor([BASE_LOSS_DICT['hard_triplet_loss']]),
-                        weight=torch.cuda.FloatTensor([self.valid_loss_dict['tri_loss{}'.format(i)]])
-                    )
-                if 'ls_loss{}'.format(i) in self.valid_loss_dict:
-                    loss_dict['{}ls_loss{}'.format(self.flag, i)] = dict(
-                        params=[feat, data_dict['label'][:, i]],
-                        type=torch.cuda.LongTensor([BASE_LOSS_DICT['lifted_structure_loss']]),
-                        weight=torch.cuda.FloatTensor([self.valid_loss_dict['ls_loss{}'.format(i)]])
-                    )
+        feat = self.embed(x) if self.embed else x
+        for i in range(len(self.linear_list)):
+            if 'tri_loss{}'.format(i) in self.valid_loss_dict:
+                loss_dict['{}tri_loss{}'.format(self.flag, i)] = dict(
+                    params=[feat, data_dict['label'][:, i]],
+                    type=torch.cuda.LongTensor([BASE_LOSS_DICT['hard_triplet_loss']]),
+                    weight=torch.cuda.FloatTensor([self.valid_loss_dict['tri_loss{}'.format(i)]])
+                )
+            if 'ls_loss{}'.format(i) in self.valid_loss_dict:
+                loss_dict['{}ls_loss{}'.format(self.flag, i)] = dict(
+                    params=[feat, data_dict['label'][:, i]],
+                    type=torch.cuda.LongTensor([BASE_LOSS_DICT['lifted_structure_loss']]),
+                    weight=torch.cuda.FloatTensor([self.valid_loss_dict['ls_loss{}'.format(i)]])
+                )
 
         return out_dict, label_dict, loss_dict
