99fd6359dba868fe1ae9176e3003968efca960e6
Donny You <youansheng@pku.edu.cn>
2019-12-18 15:51:24 +0800

    fix pose.

--- scripts/pose/coco/run_openpose_vgg19_coco_pose.sh
+++ scripts/pose/coco/run_openpose_vgg19_coco_pose.sh
@@ -24,7 +24,7 @@ if [[ ! -d ${LOG_DIR} ]]; then
 fi
 
 if [[ "$1"x == "train"x ]]; then
-  ${PYTHON} -u main.py --config_file ${CONFIG_FILE} --phase train --gpu 0 1 2 3 \
+  ${PYTHON} -u main.py --config_file ${CONFIG_FILE} --phase train --gpu 1 3 \
                        --data_dir ${DATA_DIR} --loss_type ${LOSS_TYPE} --model_name ${MODEL_NAME} \
                        --checkpoints_name ${CHECKPOINTS_NAME} --pretrained ${PRETRAINED_MODEL}  2>&1 | tee ${LOG_FILE}
 
@@ -38,10 +38,10 @@ elif [[ "$1"x == "val"x ]]; then
   ${PYTHON} -u main.py --config_file ${CONFIG_FILE} --phase test --model_name ${MODEL_NAME} \
                        --phase test --gpu 0 --resume ./checkpoints/pose/coco/${CHECKPOINTS_NAME}_latest.pth \
                        --test_dir ${DATA_DIR}/val/image --out_dir val  2>&1 | tee -a ${LOG_FILE}
-  cd metrics/det/
-  ${PYTHON} -u voc_evaluator.py --config_file "../../../"${CONFIG_FILE} \
-                                --json_dir ../../../out/results/voc/test_dir/${CHECKPOINTS_NAME}/val/json \
-                                --gt_dir ${DATA_DIR}/val/label  2>&1 | tee -a "../../"${LOG_FILE}
+  cd metric/pose/
+  ${PYTHON} -u coco_evaluator.py --config_file "../../../"${CONFIG_FILE} \
+                                 --json_dir ../../../out/results/voc/test_dir/${CHECKPOINTS_NAME}/val/json \
+                                 --gt_dir ${DATA_DIR}/val/label  2>&1 | tee -a "../../"${LOG_FILE}
 
 else
   echo "$1"x" is invalid..."
